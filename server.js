98a960fa6123b6b1a6a3a111b6c52375b898b38e
Harman <harmanelins@gmail.com>
2022-07-19 03:07:21 +0700

    menambah console.log pada listen

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 // const io = require("socket.io")(server);
-server.listen(port);
+server.listen(port, () => {
+  console.log(`terhubung pada port ${port}`);
+});
 // io.on("connection", (stream) => {
 //   console.log("user connected");
 // });
